a1c93457ec7900edc5fc022fa1b21496737d3873
sunilkumarpv <sunilkumarpv@gmail.com>
2018-02-21 12:07:01 +0530

    x

---
+++ Input_processing.R
@@ -0,0 +1,20 @@
+# Remove Duplicates form raw LD data
+
+require(data.table) 
+LD_NoDup <- Raw_LD_human[!duplicated(rleidv(Raw_LD_human, cols = c("LncRNA", "Disease"))), ]
+
+# Subset DOID to match with LD
+
+DOID_LD_Common <- subset(DOID, DOID$`Preferred Label` %in% LD_NoDup$Disease)
+
+# Retain interactions of diseases having DOID in LD
+
+LD_Final <- subset(LD_NoDup, LD_NoDup$Disease %in% DOID_LD_Common$`Preferred Label`)
+
+# LncRNA, Disease and DOID in a single file.. Organism coloumn will be replaced by DOID column
+
+LDFinal_vector <- LD_Final[['Disease']]
+
+for(i in 1:nrow(LD_Final))
+  LD_Final[i,3] <- DOID[which(LDFinal_vector[i] == DOID$`Preferred Label`),1]
+colnames(LD_Final)[3] <- "DOID"
